0519b40e32aeac493d0e2ca6e965307e4ae172bf
OhPandey <45360648+OhPandey@users.noreply.github.com>
2023-02-20 17:02:39 +0100

    Implemented seat assignment

--- Student.py
+++ Student.py
@@ -1,11 +1,14 @@
 # Mock class atm
+import random
+
+
 class Student:
 
     def __init__(self, name, birth, id):
         self.name = name
         self.birth = birth
         self.id = id
-        self.seat = "NYI"
+        self.seat = None
 
     def getName(self):
         return self.name
@@ -16,8 +19,17 @@ class Student:
     def getId(self):
         return self.id
 
-    def setSeat(self, seat):
-        self.seat = seat
-
     def getSeat(self):
         return self.seat
+
+    def assignSeat(self, seatlist, maxseat):
+        # Get a random seatnumber
+        seat = random.randint(0, maxseat - 1)
+
+        # Reroll if its already used
+        while seat not in seatlist:
+            seat = random.randint(0, maxseat - 1)
+
+        self.seat = seat+1
+        seatlist.remove(seat)
+        return seatlist
--- main.py
+++ main.py
@@ -1,4 +1,38 @@
 from Console import Console
+from Student import Student
+
+#AssingSeat Testing
+
+# Set available seats
+maxseats = 5
+
+seatList = list(range(0, maxseats))
+
+student1 = Student("Joey", "/", "/")
+
+seatList = student1.assignSeat(seatList, maxseats)
+
+student2 = Student("Joey", "/", "/")
+
+seatList = student2.assignSeat(seatList, maxseats)
+
+student3 = Student("Joey", "/", "/")
+
+seatList = student3.assignSeat(seatList, maxseats)
+
+student4 = Student("Joey", "/", "/")
+
+seatList = student4.assignSeat(seatList, maxseats)
+
+student5 = Student("Joey", "/", "/")
+
+seatList = student5.assignSeat(seatList, maxseats)
+
+print(student1.getSeat())
+print(student2.getSeat())
+print(student3.getSeat())
+print(student4.getSeat())
+print(student5.getSeat())
 
 
 # Console
